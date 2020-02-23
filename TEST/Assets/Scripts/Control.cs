00f5014149bd925daf9cc716f1d41a4c8d8f5eea
BryanTG1221 <61299735+BryanTG1221@users.noreply.github.com>
2020-02-22 21:03:03 -0600

    Veriosn 0.0.1
    Primera version

---
+++ TEST/Assets/Scripts/Control.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float Yincremento;
+    public float speed;
+    public float minHeight;
+    public float maxHeight;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed*Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincremento);
+            
+        }else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincremento);
+                    }
+    }
+}
